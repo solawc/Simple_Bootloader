4e7e9cdd0782a7933f8a3c6fc6cee3ebc98d9382
Solawc <1225093964@qq.com>
2023-12-20 14:11:59 +0800

    调整GPIO驱动

--- src/hal/mcu/stm32/gpio.c
+++ src/hal/mcu/stm32/gpio.c
@@ -28,12 +28,81 @@ GPIO_TypeDef * const digital_regs[] = {
 #endif
 };
 
-static int
-regs_to_pin(GPIO_TypeDef *regs, uint32_t bit)
+// static int regs_to_pin(GPIO_TypeDef *regs, uint32_t bit)
+// {
+//     int i;
+//     for (i=0; i<ARRAY_SIZE(digital_regs); i++)
+//         if (digital_regs[i] == regs)    // 查找对应的GPIO端口, bit用于计算对应的GPIO引脚
+//             return GPIO('A' + i, ffs(bit)-1);
+//     return 0;
+// }
+
+// Verify that a gpio is a valid pin
+static int gpio_valid(uint32_t pin)
 {
-    int i;
-    for (i=0; i<ARRAY_SIZE(digital_regs); i++)
-        if (digital_regs[i] == regs)
-            return GPIO('A' + i, ffs(bit)-1);
-    return 0;
+    uint32_t port = GPIO2PORT(pin);
+    return port < ARRAY_SIZE(digital_regs) && digital_regs[port];
+}
+
+struct gpio_out gpio_out_setup(uint32_t pin, uint32_t val)
+{
+    if (!gpio_valid(pin)) 
+        DEBUG_PRINT("Not an output pin!");
+
+    GPIO_TypeDef *regs = digital_regs[GPIO2PORT(pin)];
+
+    struct gpio_out g = { .regs=regs, .bit=GPIO2BIT(pin) };
+
+    GPIO_InitTypeDef GPIO_Init = {
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pin = pin,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_VERY_HIGH,
+    };
+
+    HAL_GPIO_Init(g.regs, &GPIO_Init);
+
+    return g;
+}
+
+void gpio_out_toggle_noirq(struct gpio_out g)
+{
+    GPIO_TypeDef *regs = g.regs;
+    regs->ODR ^= g.bit;
+}
+
+void gpio_out_write(struct gpio_out g, uint32_t val)
+{
+    GPIO_TypeDef *regs = g.regs;
+    if (val)
+        regs->BSRR = g.bit;
+    else
+        regs->BSRR = g.bit << 16;
+}
+
+struct gpio_in gpio_in_setup(uint32_t pin, uint32_t val)
+{
+    if (!gpio_valid(pin)) 
+        DEBUG_PRINT("Not an input pin!");
+
+    GPIO_TypeDef *regs = digital_regs[GPIO2PORT(pin)];
+
+    struct gpio_in g = { .regs=regs, .bit=GPIO2BIT(pin) };
+
+    GPIO_InitTypeDef GPIO_Init = {
+        .Mode = GPIO_MODE_INPUT,
+        .Pin = pin,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_VERY_HIGH,
+    };
+
+    HAL_GPIO_Init(g.regs, &GPIO_Init);
+
+    return g;
+}
+
+uint8_t gpio_in_read(struct gpio_in g)
+{
+    GPIO_TypeDef *regs = g.regs;
+    return !!(regs->IDR & g.bit);
 }
--- src/hal/mcu/stm32/gpio.h
+++ src/hal/mcu/stm32/gpio.h
@@ -1,12 +1,24 @@
- #ifndef _gpio_h
- #define _gpio_h
+#ifndef _gpio_h
+#define _gpio_h
 
- #include "../../../main.h"
+#include "../../../main.h"
+
+extern GPIO_TypeDef * const digital_regs[];
+#define GPIO(PORT, NUM) (((PORT)-'A') * 16 + (NUM))
+#define GPIO2PORT(PIN) ((PIN) / 16)
+#define GPIO2BIT(PIN) (1<<((PIN) % 16))
+
 
 struct gpio_out {
     void *regs;
     uint32_t bit; 
 };
+
+struct gpio_in {
+    void *regs;
+    uint32_t bit; 
+};
+
 
 
 
